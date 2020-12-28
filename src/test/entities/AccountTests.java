4f7046ce4541fb493b8e1abb949f918a450a3c92
pierreEnoc <pierre_enoc@yahoo.fr>
2020-12-28 11:58:59 -0300

    :test withdraw clean and return new balance, test method withdraw

--- src/test/entities/AccountTests.java
+++ src/test/entities/AccountTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 	
@@ -12,7 +13,7 @@ public class AccountTests {
 		
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account acc = new Account(1L, 0.0);
+		Account acc = AccountFactory.createEmptyAccount();
 		
 		acc.deposit(amount);
 		
@@ -22,12 +23,45 @@ public class AccountTests {
 	@Test
 	public void depositShouldDoNothingWhenNegayiveAmount() {
 		double expectedValue = 100.0;
-		Account acc = new Account(1L, expectedValue);
+		Account acc = AccountFactory.createAccount(expectedValue);
 		double amount = -200.00;
 		
 		acc.deposit(amount);
 		
-		Assertions.assertEquals(expectedValue, acc.getBalance());
+		Assertions.assertEquals(expectedValue, acc.getBalance());	
+	}
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
+	
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSuffientBalance() {
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		acc.withdraw(500.00);
+		
+		Assertions.assertEquals(300.0, acc.getBalance());
+	}
+	
+	@Test
+	public void withdrawShouldThrowExceptionWhenInsufficientBalnace() {
+		double initialBalance = 800.0;
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account acc = AccountFactory.createAccount(initialBalance);
+			acc.withdraw(801.0);
+		});
+		
 		
 	}
 
--- src/tests/factory/AccountFactory.java
+++ src/tests/factory/AccountFactory.java
@@ -1,5 +1,15 @@
 package tests.factory;
 
+import entities.Account;
+
 public class AccountFactory {
+	
+	public static Account createEmptyAccount() {
+		return new Account(1L, 0.0);
+	}
+	
+	public static Account createAccount(double initailBalance) {
+		return new Account(1L, initailBalance);
+	}
 
 }
